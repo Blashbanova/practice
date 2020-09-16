d141b3b62842e561a50023ab966c72ab1324c304
Blashbanova <blashbanovaa@mail.ru>
2020-09-16 22:04:00 +0400

    Созданы классы Person и Program

---
+++ src/main/java/ru/ssau/tk/blashbanoba/practice/Person.java
@@ -0,0 +1,32 @@
+package ru.ssau.tk.blashbanoba.practice;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int passportld;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportld() {
+        return passportld;
+    }
+
+    public void setPassportld(int passportld) {
+        this.passportld = passportld;
+    }
+}
---
+++ src/main/java/ru/ssau/tk/blashbanoba/practice/Program.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.blashbanoba.practice;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Person Tom = new Person();
+        Person Harry=new Person();
+
+        Tom.setFirstName("Tom");
+        Tom.setLastName("Hardy");
+        Tom.setPassportld(658393);
+
+        Harry.setFirstName("Harry");
+        Harry.setLastName("Potter");
+        Harry.setPassportld(957483);
+
+    }
+}
