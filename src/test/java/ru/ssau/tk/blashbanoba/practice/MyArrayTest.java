f10601022ac0fdecf9c90bef822602703741b2d8
Blashbanova <blashbanovaa@mail.ru>
2020-12-06 11:52:12 +0400

    2.12 Добавлен тест

--- src/test/java/ru/ssau/tk/blashbanoba/practice/MyArrayTest.java
+++ src/test/java/ru/ssau/tk/blashbanoba/practice/MyArrayTest.java
@@ -103,21 +103,14 @@ public class MyArrayTest {
         assertEquals(array.getArrayDivisors(-7), new int[]{1, -1, -7, 7});
     }
 
-    public LinkedList<Integer> arraySimpleNumbers(int c) {
-        if (c < 2) return new LinkedList<Integer>();
-        LinkedList<Integer> primes = new LinkedList<Integer>();
-        LinkedList<Integer> nums = new LinkedList<Integer>();
-        for (int i = 2; i <= c; i++) {
-            nums.add(i);
-        }
-        while (nums.size() > 0) {
-            int nextPrime = nums.remove();
-            for (int i = nextPrime * nextPrime; i <= c; i += nextPrime) {
-                nums.removeFirstOccurrence(i);
-            }
-            primes.add(nextPrime);
-        }
-        return primes;
+    @Test
+    public void testArraySimpleNumbers() {
+        MyArray array = new MyArray();
+        LinkedList<Integer> checkPrimes = new LinkedList<Integer>();
+        checkPrimes.add(2);
+        checkPrimes.add(3);
+        checkPrimes.add(5);
+        assertEquals(array.arraySimpleNumbers(5), checkPrimes);
     }
 }
 
