6ecf4535826249e2375f0bacad387712c9043b73
Blashbanova <blashbanovaa@mail.ru>
2020-12-25 21:28:48 +0400

    3.2 у русских символов отрицательные значения

--- src/main/java/ru/ssau/tk/blashbanoba/practice/MyString.java
+++ src/main/java/ru/ssau/tk/blashbanoba/practice/MyString.java
@@ -2,9 +2,23 @@ package ru.ssau.tk.blashbanoba.practice;
 
 public class MyString {
 
-    public void outputLine (String string){
+    public void outputLine(String string) {
         for (int i = 0; i < string.length(); i++) {
             System.out.println(string.charAt(i));
         }
+    }
+
+    public void getArrayOfBytes(String str) {
+        byte[] bytesValue = str.getBytes();
+        for (int bytes : bytesValue) {
+            System.out.println(bytes);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyString string = new MyString();
+        string.getArrayOfBytes("проверка");
+        System.out.println(" ");
+        string.getArrayOfBytes("something");
     }
 }
